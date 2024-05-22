56280579eaf6bed6b6ba0bb5c31f416f5dfc5b77
gudals9677 <gudals9677@gmail.com>
2024-05-23 03:20:40 +0900

    header footer

--- board-front/src/App.tsx
+++ board-front/src/App.tsx
@@ -1,20 +1,44 @@
-import React, { useState } from 'react';
+import Footer from 'layouts/Footer';
 import './App.css';
-import { commentListMock, favoriteListMock, latestBoardListMock, top3BoardListMock } from 'mocks';
-import BoardItem from 'components/BoardItem';
-import Top3Item from 'components/Top3Item';
-import CommentItem from 'components/CommentItem';
-import FavoriteItem from 'components/FavoriteItem';
-import InputBox from 'components/InputBox';
+import { Route, Routes } from 'react-router-dom';
+import Main from 'views/Main';
+import Authentication from 'views/Authentication';
+import Search from 'views/Search';
+import User from 'views/User';
+import BoardDetail from 'views/Board/Detail';
+import BoardWrite from 'views/Board/Write';
+import BoardUpdate from 'views/Board/Update';
+import Container from 'layouts/Container';
+import { AUTH_PATH, BOARD_DETAIL_PATH, BOARD_PATH, BOARD_UPDATE_PATH, BOARD_WRITE_PATH, MAIN_PATH, SEARCH_PATH, USER_PATH } from 'constant';
 
 
+
+//         component: Application 컴포넌트        //
 function App() {
-
-  const [value, setValue] = useState<string>('');
+  //         render: Application 컴포넌트 렌더링        //
+  //description: 메인화면 : '/' - Main //
+  //description: 로그인 + 회원가입 : '/auth' - Authentication //
+  //description: 검색 화면: '/serach/:searchWord' - Search //
+  //description: 유저 페이지 : '/user/:userEmail' - User //
+  //description: 게시물 상세보기 : '/board/detail/:boardNumber' - BoardDetail //
+  //description: 게시물 작성하기 : '/board/write' - BoardWrite //
+  //description: 게시물 수정하기 : '/board/update/:boardNumber' - BoardUpdate //
+  
   return (
-    <>
-      <InputBox label='이메일' type='text' placeholder='이메일 주소를 입력해주세요' value={value} error={false} setValue={setValue} message='aaaa'/>
-    </>
+    <Routes>
+      <Route element={<Container />}>
+        <Route path={MAIN_PATH()} element={<Main />}/>
+        <Route path={AUTH_PATH()} element={<Authentication />}/>
+        <Route path={SEARCH_PATH(':searchWord')} element={<Search />}/>
+        <Route path={USER_PATH(':userEmail')} element={<User />}/>
+        <Route path={BOARD_PATH()}>
+          <Route path={BOARD_WRITE_PATH()} element={<BoardWrite />}/>
+          <Route path={BOARD_DETAIL_PATH(':boardNumber')} element={<BoardDetail />}/> 
+          <Route path={BOARD_UPDATE_PATH(':boardNumber')} element={<BoardUpdate />}/>
+        </Route>
+        <Route path='*' element={<h1>404 Not Found</h1>}/>
+      </Route>
+    </Routes>
   );
 }
 
